9d8f8f852af6381ca2d2ec7755e95553f871af26
ingabiremariesandrine187 <ingabiremariesandrine187@gmail.com>
2025-09-28 00:09:49 +0200

    use addeventListener for toggleComplete deleteTask and editTask and also append child for the new list

--- index.js
+++ index.js
@@ -62,6 +62,29 @@ const clearStorageBtn = document.getElementById('clear-storage');
     let filteredTasks = tasks;
     if (filter === 'completed') filteredTasks = tasks.filter(t =>t.status === 'completed');
      if (filter === 'pending') filteredTasks = tasks.filter(t => t.status === 'pending');
+     filteredTasks.forEach(task => {
+      const li = document.createElement('li');
+      li.className = `p-3 bg-white rounded-lg shadow flex justify-between items-center transition hover:shadow-md
+        ${task.status === 'completed' ? 'line-through text-slate-400 bg-slate-100' : ''}`;
+      
+      li.innerHTML = ` 
+        <div class="flex flex-col">
+          <span class="task-name cursor-pointer">${task.name}</span>
+          <span class="text-xs text-slate-500">Due: ${fmtDate(task.dueDate)}</span>
+        </div>
+        <div class="flex gap-2 items-center">
+          <button class="toggle-btn px-2 py-1 rounded text-white ${task.status==='completed'?'bg-amber-500':'bg-emerald-600'}">
+            ${task.status==='completed'?'Undo':'Done'}
+          </button>
+          <button class="edit-btn px-2 py-1 rounded border">Edit</button>
+          <button class="delete-btn px-2 py-1 rounded border text-red-500">Delete</button>
+        </div>
+      `;
+      li.querySelector('toggle-btn').addEventListener('click', () => toggleComplete(task.id));
+      li.querySelector('delete-btn').addEventListener('click', () => deleteTask(task.id));
+      li.querySelector('.edit-btn').addEventListener('click' , () => editTask(task.id));
+      taskListEl.appendChild(li);
+     });
  }
  
 
