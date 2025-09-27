8d87301eee48b296e4d6c92f580f81bbb11dd25f
ingabiremariesandrine187 <ingabiremariesandrine187@gmail.com>
2025-09-27 23:17:24 +0200

    start function which can have all codes of js and also set Date of tasks by changing them into readable format

---
+++ index.js
@@ -0,0 +1,37 @@
+(function() {
+function uid() {
+    return Date.now().to String(36) +Math.random().toString(36).slice(2,8);
+}
+function fmtDate(iso){
+    if(!iso) return '';
+    return new Date(iso).toLocaleDateString();
+}
+function addDaysISO(n) {
+    const d = new Date();
+    d.setDate(d.getDate() +n);
+    return d.toISOString().split('T')[0];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+)
